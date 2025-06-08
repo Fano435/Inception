618c0cd19bccd3f1c2a5dc0fd3185b1b4a46f6ca
Fano435 <rasamimananajean@gmail.com>
2025-06-08 18:31:23 +0200

    Ajout adminer

---
+++ srcs/requirements/bonus/adminer/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p /var/run/php
+sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/g' /etc/php83/php-fpm.d/www.conf
+
+mkdir -p /var/www/html
+wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
+chown www-data:www-data /var/www/html/adminer.php
+chmod 755 /var/www/html/adminer.php
+
+php-fpm83 -F
