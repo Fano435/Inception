c255c96905dcb1ba34279b4abdc9d79a05498f14
Jean Rasamimanana <jrasamim@f1r4s14.clusters.42paris.fr>
2025-06-10 22:15:38 +0200

    Version rendu

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#wp-cli installation
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -11,17 +10,18 @@ chmod -R 755 /var/www/wordpress/
 
 sleep 10
 
-wp core download --allow-root
-if [ ! -f wp-config.php ]; then
-    wp core config create --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
-fi
+wp core download --locale=fr_FR --allow-root
+wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --locale="fr_FR" --allow-root
 
-wp core install --url="https://localhost" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+wp core install --url="https://$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
 wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 
 wp config set WP_REDIS_HOST redis --allow-root
 wp config set WP_REDIS_PORT 6379 --allow-root
 wp config set WP_CACHE true --allow-root
+
+wp language core install fr_FR --allow-root
+wp language core activate fr_FR --allow-root
 
 wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
